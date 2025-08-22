3d735436d0c86292912e0324a68c6e639119b99f
Fabiola Villatoro <fabiolamariavillatoro@gmail.com>
2025-08-22 17:32:02 -0400

    rewrite

--- send_email.py
+++ send_email.py
@@ -1,41 +1,24 @@
-# -*- coding: utf-8 -*-
-#
-# send_email.py
-# This script sends an email reminder using yagmail.
-#
-
 import yagmail
 import os
 import datetime
 
-# Get the email password from the GitHub Actions environment variable
 password = os.environ.get('EMAIL_PASSWORD')
-
-# Set up the sender and recipient. Use your email address for both.
 sender = 'fvillatoro99@gmail.com'
 recipient = 'fvillatoro99@gmail.com'
 
-# Get the current date to include in the email subject
 today = datetime.date.today().strftime('%B %d, %Y')
 
-# Define the subject and body of the email.
-# We are no longer using .encode() here.
-subject = f'Daily Cold Plunge Reminder for {today}'
+subject = f'DAILY COLD PLUNGE REMINDER FOR {today}'
 body = """
-Hey there,
 
-DID YOU DO YOUR COLD PLUNGE?!?!?!?
+test
 
-Love,
-yourself
 """
 
-# Try to initialize yagmail and send the email
 try:
     yag = yagmail.SMTP(sender, password)
     yag.send(to=recipient, subject=subject, contents=body)
-    print("Email sent successfully!")
+    print("Email sent success")
 
 except Exception as e:
-    # If an error occurs, print it to the GitHub Actions log
-    print(f"Error sending email: {e}")
+    print("Error sending email: {e}")
